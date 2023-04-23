c85dc867395353b76554f0a22edf0239aa204dc2
Khlorz <105799772+khlorz@users.noreply.github.com>
2023-04-23 11:16:16 +0800

    Improved demo files

--- v1/demo.h
+++ v1/demo.h
@@ -1,3 +1,9 @@
 // Add this header file to enable use of ComboAutoSelect demo function
 
-void ComboAutoSelectDemo();
+namespace ImGui
+{
+
+void ShowComboAutoSelectDemo(bool* p_open = nullptr);
+void ShowComboFilterDemo(bool* p_open = nullptr);
+
+}
